d96efb0c039789f000172eb2b392e09724e8977b
TheLowestByte <thelowestbyte@gmail.com>
2023-09-07 13:22:54 -0400

    added spite script

--- Assets/Scripts/DirectionalNode.cs
+++ Assets/Scripts/DirectionalNode.cs
@@ -10,31 +10,27 @@ public class DirectionalNode : MonoBehaviour
     public string current;
     SpriteRenderer sr;
 
-    // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         SwitchSprite();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void SwitchSprite()
     {
         if(current == "left")
         {
+            Debug.Log("Choose Left");
             sr.sprite = left;
         }
         else if(current == "right")
         {
+            Debug.Log("Choose Right");
             sr.sprite = right;
         }
         else
         {
+            Debug.Log("Choose None");
             sr.sprite = none;
         }
     }
